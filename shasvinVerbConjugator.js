3521a9cad341961128cee97bdfc9610b0eff8204
Pau Roca <paurocapau@gmail.com>
2024-01-31 02:24:28 +0100

    added conjugation for personal forms
    the conjugator generates the tables, as of now there are breaks for each new form isntead of /. i still need to add impersonal forms and format the tables with CSS

--- shasvinVerbConjugator.js
+++ shasvinVerbConjugator.js
@@ -3,7 +3,7 @@ var verbsAndGroup = {
     "upalil":"2.P",
     "ajul":"3.1"
 }
-
+//[past, future, imperfective]
 var tenses = {
   "PRS": [0, 0, 0],
   "HAB": [0, 0, 1],
@@ -20,22 +20,9 @@ var perfective_equivalents = {
   "H.FUT": "FUT",
   "H.COND": "COND"
 };
-var voiced_equivalents = {
-  "p": "b",
-  "t": "d",
-  "k": "g"
-};
-var pronouns = {
-  "ang": 0,
-  "ngak": 1,
-  "ip": 2,
-  "se": 3,
-  "ane": 4,
-  "anak": 5,
-  "akje": 6,
-  "pip": 7,
-  "sese": 8
-};
+var voiced_equivalents = {"p": "b", "t": "d", "k": "g"};
+var pronouns = {"ang": 0, "ngak": 1, "ip": 2, "se": 3, "ane": 4, "anak": 5, "akje": 6, "pip": 7, "sese": 8};
+
 var conj_one_one_np = ["a", "kak", "ip", "s", "ane", "anak", "kje", "pi", "ase"];
 var conj_one_two_np = ["e", "kak", "ip", "s", "ene", "enak", "kje", "pi", "ese"];
 var conj_one_three_np = ["e", "kak", "ip", "s", "ane", "anak", "kje", "pi", "ese"];
@@ -46,7 +33,7 @@ var conj_three_two_np = ["of", "uk/ukak", "up", "us", "one", "onak", "ukje", "up
 var conj_one = [conj_one_one_np, conj_one_two_np, conj_one_three_np];
 var conj_two = [conj_two_P_np, conj_two_G_np];
 var conj_three = [conj_three_one_np, conj_three_two_np];
-var non_past = [conj_one, conj_two, conj_three];
+var conjugating_suffixes = [conj_one, conj_two, conj_three];
 
 function updateTitle(verb){
     var group = verbsAndGroup[verb];
@@ -66,9 +53,12 @@ function conjugateVerb(){
         document.getElementById("tables").innerHTML = "";
         return;
     } 
-    var stem = verb.slice(0,-2);
-    
-    document.getElementById("tables").innerHTML = get_post_reduced_form(stem);
+    var tables = conjugate_all_tenses(verb,verbsAndGroup[verb]);
+    var tableString = "";
+    for(let i = 0; i < tables.length; i += 2){
+        tableString += make_table_pair(tables[i],tables[i+1]);
+    }
+    document.getElementById("tables").innerHTML = tableString;
 }
 
 //gets two variables group and subgroup from a string of the type A.B
@@ -77,26 +67,31 @@ function get_group(group){
     var subgroup = indexes[1];
     if(subgroup == 'P') subgroup = '1';
     if(subgroup == 'G') subgroup = '2';
-    return [int(indexes[0],int(subgroup))]
+    return [parseInt(indexes[0]),parseInt(subgroup)]
 }
-//given an array of rows, makes a simple html table with the given values
-function makeTable(rows){
+//given an array of rows, makes a simple html table with the given values and a title
+function make_table(title, rows){
     var content = "<table cellspacing='2px'>\n<tbody>\n";
-    for(let r in rows){
+    content += '<th colspan="4">' + title + '</th>\n</tbody>\n';
+    for(let r of rows){
         var rowToPaste = "";
-        for(let el in rows[r]){
-            rowToPaste += "<td>" + rows[r][el] + "</td>\n";
+        for(let el of r){
+            rowToPaste += "<td>" + el + "</td>\n";
         }
         content += "<tr>\n" + rowToPaste + "</tr>\n";
     }
     content += "\n</tbody>\n</table>\n";
     return content;
 }
+//returns a div class="table-pair" with two given tables
+function make_table_pair(table1, table2){
+    return '<div class="table-pair">\n' + table1 + table2 + '</div>';
+}
 function is_vowel(c){
     return is_in(c,['a','e','i','o','u']);
 }
 function is_in(c, array){
-    for(let i in array) if(c == array[i]) return true;
+    for(let i of array) if(c == i) return true;
     return false;
 }
 //gets the post reduced form of a verb from its stem
@@ -107,9 +102,9 @@ function get_post_reduced_form(stem){
     var found = false;
     var reversedStem = new_str.split('').reverse().join('');
     //read backwars until a second vowel is found
-    for(let c in reversedStem){
-        if(is_vowel(reversedStem[c]) && !found) found = true;
-        else if(is_vowel(reversedStem[c])) break;
+    for(let c of reversedStem){
+        if(is_vowel(c) && !found) found = true;
+        else if(is_vowel(c)) break;
         count += 1;
     }
     //select the right substring with only one vowel
@@ -124,7 +119,7 @@ function get_post_reduced_form(stem){
 }
 //returns the stem with the infix -o- applied and updates the subgroup
 //to trick the rest of the code to apply the correct suffix
-function apply_past_o(stem,group,subgroup,pronoun){
+function apply_past_o(stem, group, subgroup, pronoun){
     //if its 2.G we change it to 2.P, if its 3.2, to 3.1
     if(group == 2 || group == 3) return [stem+'o',group,1];
     //if its 1.3 with ang we change it to 1.1 so we get oa and not oe
@@ -135,4 +130,87 @@ function apply_past_o(stem,group,subgroup,pronoun){
     if(pronoun == 'pip') return [stem+'o',2,1];
     //else we just add the infix
     return [stem+'o',group,subgroup];
+}
+//applies a suffix to a stem and may or not apply voicing
+function apply_suffix(stem, suffix, voicing = false){
+    //if there are more than one suffix compute both
+    if(suffix.includes('/')){
+        var split = suffix.split('/');
+        return apply_suffix(stem, split[0], voicing)  + '<br>' + apply_suffix(stem, split[1], voicing);
+    }
+    //if voicing is true, apply the voicing if needed
+    if(voicing && suffix.length > 1){
+        //stem ends in a vowel and suffix starts with <PV>
+        if(is_vowel(stem.charAt(stem.length-1)) && is_in(suffix.charAt(0),['p','t','k']) && is_vowel(suffix.charAt(1))){
+            var new_suffix = voiced_equivalents[suffix.charAt(0)] + suffix.slice(1);
+            return stem + new_suffix;
+        }
+        //stem ends in <VP> and the suffix starts in a vowel
+        if(is_vowel(stem.charAt(stem.length-2)) && is_in(stem.charAt(stem.length-1),['p','t','k']) && is_vowel(suffix.charAt(0))){
+            var new_stem = stem.slice(0,-1) + voiced_equivalents[stem.charAt(stem.length-1)];
+            return new_stem + suffix;
+        }
+    }
+    //resolve <PP> clusters
+    if(is_in(stem.charAt(stem.length-1),['p','t','k']) && is_in(suffix.charAt(0),['p','t','k']) && stem.charAt(stem.length-1) != suffix.charAt(0)){
+        return stem.slice(0,-1) + suffix;
+    }
+    //else just add the suffix normally
+    return stem + suffix;
+}
+//returns the correct form(s) of the verb for one person (and number) in a specified tense
+function conjugate_one_tense_one_person(stem, group, pronoun, tense){
+    //get the group and subgroup as separate variables
+    var cj_group, cj_subgroup; [cj_group,cj_subgroup] = get_group(group);
+    //get the ID for the pronoun (0-8)
+    var person_and_number = pronouns[pronoun];
+    //get the post reduced form
+    var post_reduced = get_post_reduced_form(stem);
+
+    var conjugated_form = stem;
+    //if its imperfective, reduplicate (meso-reduplication)
+    if(tenses[tense][2] == 1) conjugated_form = apply_suffix(conjugated_form,post_reduced,true);
+    //if its future, add -kan-
+    if(tenses[tense][1] == 1) conjugated_form = apply_suffix(conjugated_form,'kan');
+    //if tis past, add -o-
+    if(tenses[tense][0] == 1) [conjugated_form,cj_group,cj_subgroup] = apply_past_o(conjugated_form,cj_group,cj_subgroup,pronoun);
+
+    //add person suffix
+    conjugated_form = apply_suffix(conjugated_form, conjugating_suffixes[cj_group-1][cj_subgroup-1][person_and_number]);
+
+    //if its imperfective, make post-reduplication by conjugating in perfective followed by post reduced form
+    if(tenses[tense][2] == 1){
+        var post_reduplicated = conjugate_one_tense_one_person(stem,group,pronoun,perfective_equivalents[tense]);
+        //check if two suffixes were applied
+        if(post_reduplicated.includes('<br>')){
+            var two_forms = post_reduplicated.split('<br>');
+            conjugated_form += '<br>' + two_forms[0] + ' ' + post_reduced + '<br>' + two_forms[1] + ' ' + post_reduced;
+        }
+        else{
+            conjugated_form += '<br>' + post_reduplicated + ' ' + post_reduced;
+        }
+    }
+    return conjugated_form;
+}
+//returns a table with the entire conjugation for a given tense
+function conjugate_one_tense(stem, group, tense, title){
+    var table_values = [];
+    var pronoun_pairs = [['ang','ane'],['---','anak'],['ngak','akje'],['ip','pip'],['se','sese']];
+    //treat pronouns as pairs to have them in the same table row
+    for(let pair of pronoun_pairs){
+        if(pair[0] == '---') table_values.push(['---','---',pair[1],conjugate_one_tense_one_person(stem,group,pair[1],tense)])
+        else{
+            var row = [pair[0],conjugate_one_tense_one_person(stem,group,pair[0],tense),pair[1],conjugate_one_tense_one_person(stem,group,pair[1],tense)];
+            table_values.push(row);
+        }
+    }
+    return make_table(title, table_values);
+}
+function conjugate_all_tenses(infinitive, group){
+    var stem = infinitive.slice(0,-2);
+    var tense_pairs = [['PRS','Present'],['HAB','Habitual'],['PRET','Preterite'],['IMPF','Imperfect'],
+                      ['FUT','Future'],['H.FUT','Habitual Future'],['COND','Conditional'],['H.COND','Habitual Conditional']];
+    var all_tenses_tables = [];
+    for(let pair of tense_pairs) all_tenses_tables.push(conjugate_one_tense(stem,group,pair[0],pair[1]));
+    return all_tenses_tables;
 }
